aa75d3d734e21d50e6d748cebcac44b7aa8fe7d9
Panos Oikonomou <po524@nyu.edu>
2025-01-16 11:00:45 -0500

    Change sddm wall

--- hypr/scripts/paper.sh
+++ hypr/scripts/paper.sh
@@ -12,10 +12,12 @@ IMAGE_PATH="$(realpath $1)"
 # Config file path
 CONFIG_FILE="$HOME/.config/hypr/hyprpaper.conf"
 LOCK_FILE="$HOME/.config/hypr/hyprlock.conf"
+SDDM_THEME="/usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds"
 
 # Clear the config file and write new configuration
 echo "preload = $IMAGE_PATH" > $CONFIG_FILE
 echo "wallpaper = ,$IMAGE_PATH" >> $CONFIG_FILE
+echo "splash = false" >> $CONFIG_FILE
 
 # Preload the image (optional if the config file is used directly)
 hyprctl hyprpaper preload "$IMAGE_PATH"
@@ -30,4 +32,7 @@ hyprpaper & disown
 # Set up Hyprlock to have the same background
 sed -i "0,/path/s/.*path.*/\t$(printf '%s' "path=$IMAGE_PATH" | sed 's/[&/\]/\\&/g')/"  $LOCK_FILE
 
-echo "Wallpaper set to $1 and configuration updated in hyprpaper.conf and hyprlock.conf"
+# Set up SDDM to have the same background
+sudo cp $IMAGE_PATH "$SDDM_THEME/1.png"
+
+echo "Wallpaper set to $1 and configuration updated in hyprpaper.confi, sddm, and hyprlock.conf"
