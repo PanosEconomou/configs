32975e0b7e255869cb04a87e45fc66037c09efaf
Panos Oikonomou <po524@nyu.edu>
2025-12-31 04:34:07 -0500

    New hyprland update chaos

--- hypr/scripts/paper.sh
+++ hypr/scripts/paper.sh
@@ -15,19 +15,10 @@ LOCK_FILE="$HOME/.config/hypr/hyprlock.conf"
 SDDM_THEME="/usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds"
 
 # Clear the config file and write new configuration
-echo "preload = $IMAGE_PATH" > $CONFIG_FILE
-echo "wallpaper = ,$IMAGE_PATH" >> $CONFIG_FILE
-echo "splash = false" >> $CONFIG_FILE
-
-# Preload the image (optional if the config file is used directly)
-hyprctl hyprpaper preload "$IMAGE_PATH"
+sed -i "s/\(path[[:space:]]*=[[:space:]]*\).*/\1$(printf '%s' "$IMAGE_PATH" | sed 's/[&/\]/\\&/g')/" $CONFIG_FILE
 
 # Set the image as the wallpaper (optional if the config file is used directly)
-hyprctl hyprpaper wallpaper ",$IMAGE_PATH"
-
-# Restart hyprpaper
-pkill hyprpaper
-hyprpaper & disown
+hyprctl hyprpaper wallpaper ", $IMAGE_PATH, cover"
 
 # Set up Hyprlock to have the same background
 sed -i "0,/path/s/.*path.*/\t$(printf '%s' "path=$IMAGE_PATH" | sed 's/[&/\]/\\&/g')/"  $LOCK_FILE
@@ -35,4 +26,4 @@ sed -i "0,/path/s/.*path.*/\t$(printf '%s' "path=$IMAGE_PATH" | sed 's/[&/\]/\\&
 # Set up SDDM to have the same background
 sudo cp $IMAGE_PATH "$SDDM_THEME/1.png"
 
-echo "Wallpaper set to $1 and configuration updated in hyprpaper.confi, sddm, and hyprlock.conf"
+echo "Wallpaper set to $1 and configuration updated in hyprpaper.conf, sddm, and hyprlock.conf"
