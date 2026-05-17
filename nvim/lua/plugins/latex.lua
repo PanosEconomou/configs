d2ebfa8f77bdebdd12c3c63bb067e219af267b3c
Panos Oikonomou <po524@nyu.edu>
2026-05-17 19:30:10 -0400

    finally added support for surrounding environments

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -1,9 +1,16 @@
 -- Latex Support
 return {
     {
-        "lervag/vimtex",
-        ft     = { "tex" },
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
         config = function()
+            require("nvim-surround").setup({})  -- activates S, ys, ds, cs globally
+        end,
+    },
+    {
+        "lervag/vimtex",
+        ft              = { "tex" },
+        config          = function()
             vim.g.tex_flavor                         = "latex"
             vim.g.vimtex_view_method                 = "zathura"
             vim.g.vimtex_quickfix_mode               = 0
@@ -20,12 +27,29 @@ return {
 
             -- VimTeX keymaps
             local map = vim.keymap.set
-            map("n", "<localleader>lc",
-                "<cmd>VimtexStop<CR><Plug>(vimtex-clean-full)",
-                { noremap = false, desc = "VimTeX clean full" })
-            map("n", "<localleader>lC",
-                "<cmd>VimtexStop<CR><Plug>(vimtex-clean)",
-                { noremap = false, desc = "VimTeX clean" })
+            map("n", "<localleader>lc","<cmd>VimtexStop<CR><Plug>(vimtex-clean-full)",  { noremap = false, desc = "VimTeX clean full" })
+            map("n", "<localleader>lC","<cmd>VimtexStop<CR><Plug>(vimtex-clean)",       { noremap = false, desc = "VimTeX clean" })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "tex",
+                callback = function()
+                    require("nvim-surround").buffer_setup({
+                        surrounds = {
+                            ["e"] = {
+                                add = function()
+                                    local env = require("nvim-surround.config").get_input("Environment: ")
+                                    if env then
+                                        return {
+                                            { "\\begin{" .. env .. "}" },
+                                            { "\\end{" .. env .. "}" },
+                                        }
+                                    end
+                                end,
+                            },
+                        },
+                    })
+                end,
+            })
         end,
     },
 }
