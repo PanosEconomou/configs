6833b064565d3c8255f567ae37ee4c1c6a8d3eb5
Panos Oikonomou <po524@nyu.edu>
2026-06-09 12:49:44 -0400

    customization

--- hypr/modules/look.lua
+++ hypr/modules/look.lua
@@ -58,6 +58,12 @@ config({
             render_power    = 3,
         },
     },
+
+    misc = {
+        force_default_wallpaper = 0,
+        disable_hyprland_logo   = true,
+        disable_splash_rendering= true,
+    },
 })
 
 rule({
